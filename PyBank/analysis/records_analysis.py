08caf2455f698d19952e2ef23357a83b66a493e7
Michelle Simek <michellesimek@michelles-mbp.attlocal.net>
2021-03-05 19:10:11 -0600

    cleaned up script, and started to write script to export analysis to a text file

--- PyBank/analysis/records_analysis.py
+++ PyBank/analysis/records_analysis.py
@@ -8,7 +8,7 @@ import csv
 csvpath = os.path.join("..", "Resources", "budget_data.csv")
 
 #function to return analysis for dataset
-def dataset_analysis(row):
+def dataset_analysis(textfile):
 
     #create list to add months
     total_months = []
@@ -18,7 +18,8 @@ def dataset_analysis(row):
     total_amount = 0
     
     #for loop to read each row in csv
-    for row in csvreader:
+    for row in csvreader: 
+        
         #add each month to total_months list
         total_months.append(row[0])
         #add profit/losses to the profit_losses list
@@ -30,35 +31,38 @@ def dataset_analysis(row):
         greatest_profit = max(profits_losses)
         greatest_lost = min(profits_losses)
 
-    
+    print(f"Finanical Analysis")
+    print('----------------------------------------')
     print(f'Total Months: {len(total_months)}')
     print(f'Total: ${total_amount}')
     print(f'Average Change: ${round(average_change,2)}')
     print(f'Greatest Increase in Profits: ${greatest_profit}')
     print(f'Greatest Decrease in Profits: ${greatest_lost}')
 
+    textfile.write(
+        "Finanical Analysis\n"
+        '----------------------------------------\n'
+        'Total Months: ' + str(len(total_months)) + '\n'
+        'Total: ' + str(total_amount) + '\n'
+        'Average Change: ${round(average_change,2)}'
+        'Greatest Increase in Profits: ${greatest_profit}'
+        'Greatest Decrease in Profits: ${greatest_lost}')
 
+#path of above script
+dataset_path = '/Users/michellesimek/Desktop/Homework/python-challenge/PyBank/analysis/'
+
+#path for creating a text file that will contain above information
+output_path = os.path.join(dataset_path, "new.txt")
+
+#open the new text file as write
+# with open(output_path, 'w', encoding = 'utf8') as txt:
+#     txt.write("hi")
+
+textfile = open(output_path, 'w', encoding = 'utf8')
 #open csvfile as read only
 with open(csvpath, 'r', encoding='utf8') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     #read header row
     csv_header = next(csvreader)
-
-    for row in csvreader:
-        print(f"Finanical Analysis")
-        print('----------------------------------------')
-        #call the dataset analysis function
-        dataset_analysis(row)
-
-#path of above script
-dataset_path = 'D:/Homework/python-challenge/PyBank/analysis/records.analysis.py'
-data = os.listdir(dataset_path)
-
-#path for creating a text file that will contain above information
-output_path = os.path.join("..", "Analysis", "new.txt")
-
-#open the new text file as write
-with open(output_path, 'w', encoding = 'utf8') as txt:
-    txt.write("hi")
-
+    dataset_analysis(textfile)
