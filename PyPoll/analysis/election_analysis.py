c77a6ac92570ab0bdc77fa891eba8bc8c8a6311c
Michelle Simek <michellesimek@michelles-mbp.attlocal.net>
2021-03-07 12:40:18 -0600

    wrote script to read csvfile and find total number of votes

---
+++ PyPoll/analysis/election_analysis.py
@@ -0,0 +1,27 @@
+import os
+import csv
+
+csvpath = os.path.join("..", "Resources", "election_data.csv")
+
+def election_analysis():
+    candidates = []
+    total_votes = []
+  
+    
+    for row in csvreader:
+        total_votes.append(row[0])
+  
+
+    print("Election Results")
+    print("---------------------")
+    print(f'Total Votes: {len(total_votes)}')
+    print("---------------------")
+
+with open(csvpath, "r", encoding="utf8") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(csvreader)
+
+    election_analysis()
+
+
